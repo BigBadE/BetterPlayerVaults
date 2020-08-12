735473fbc6366ee3e2e095b1f193d287083cb425
Restyled.io <commits@restyled.io>
2020-08-12 20:01:19 +0000

    Restyled by clang-format

--- api/src/main/java/software/bigbade/playervaults/PlayerVaults.java
+++ api/src/main/java/software/bigbade/playervaults/PlayerVaults.java
@@ -1,6 +1,13 @@
 package software.bigbade.playervaults;
 
 import co.aikar.taskchain.TaskChain;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,73 +17,64 @@ import org.bukkit.plugin.java.JavaPluginLoader;
 import software.bigbade.playervaults.api.IVaultLoader;
 import software.bigbade.playervaults.utils.FileUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public abstract class PlayerVaults extends JavaPlugin {
-    private static final URL VERSION_URL = FileUtils.getURL("https://raw.githubusercontent.com/BigBadE/BetterPlayerVaults/release/version.yml");
-    private static final String VERSION_YML = "version.yml";
-    @Getter
-    private static Logger pluginLogger;
+  private static final URL VERSION_URL = FileUtils.getURL(
+      "https://raw.githubusercontent.com/BigBadE/BetterPlayerVaults/release/version.yml");
+  private static final String VERSION_YML = "version.yml";
+  @Getter private static Logger pluginLogger;
 
-    @Setter(AccessLevel.PRIVATE)
-    private static String version;
-    @Getter
-    @Setter(AccessLevel.PRIVATE)
-    private static String latestVersion;
+  @Setter(AccessLevel.PRIVATE) private static String version;
+  @Getter @Setter(AccessLevel.PRIVATE) private static String latestVersion;
 
-    //Required for MockBukkit
-    public PlayerVaults() {
-        super();
+  // Required for MockBukkit
+  public PlayerVaults() { super(); }
+
+  // Required for MockBukkit
+  protected PlayerVaults(JavaPluginLoader loader,
+                         PluginDescriptionFile description, File dataFolder,
+                         File file) {
+    super(loader, description, dataFolder, file);
+  }
+
+  public static void setPluginLogger(Logger pluginLogger) {
+    if (PlayerVaults.pluginLogger == null) {
+      PlayerVaults.pluginLogger = pluginLogger;
     }
+  }
 
-    //Required for MockBukkit
-    protected PlayerVaults(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
-        super(loader, description, dataFolder, file);
+  public void initVersion() {
+    File versionFile = new File(getDataFolder(), VERSION_YML);
+    if (!versionFile.exists()) {
+      FileUtils.copyURLToFile(VERSION_URL, versionFile);
+      setVersion("INSTALL");
+    } else {
+      byte[] data = FileUtils.read(versionFile);
+      setVersion(
+          new String(data, 10, data.length - 11, StandardCharsets.UTF_8));
     }
-
-    public static void setPluginLogger(Logger pluginLogger) {
-        if (PlayerVaults.pluginLogger == null) {
-            PlayerVaults.pluginLogger = pluginLogger;
-        }
+    assert VERSION_URL != null;
+    try (InputStream stream = FileUtils.createStream(VERSION_URL)) {
+      byte[] buffer = new byte[256];
+      assert stream != null;
+      int read = stream.read(buffer);
+      String foundVersion =
+          new String(buffer, 10, read - 11, StandardCharsets.UTF_8);
+      if (foundVersion.equals("404: Not Found")) {
+        foundVersion = version;
+      }
+      setLatestVersion(foundVersion);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void initVersion() {
-        File versionFile = new File(getDataFolder(), VERSION_YML);
-        if (!versionFile.exists()) {
-            FileUtils.copyURLToFile(VERSION_URL, versionFile);
-            setVersion("INSTALL");
-        } else {
-            byte[] data = FileUtils.read(versionFile);
-            setVersion(new String(data, 10, data.length-11, StandardCharsets.UTF_8));
-        }
-        assert VERSION_URL != null;
-        try (InputStream stream = FileUtils.createStream(VERSION_URL)) {
-            byte[] buffer = new byte[256];
-            assert stream != null;
-            int read = stream.read(buffer);
-            String foundVersion = new String(buffer, 10, read-11, StandardCharsets.UTF_8);
-            if(foundVersion.equals("404: Not Found")) {
-                foundVersion = version;
-            }
-            setLatestVersion(foundVersion);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+  public static boolean shouldUpdate() {
+    return !version.equals(latestVersion);
+  }
 
-    public static boolean shouldUpdate() {
-        return !version.equals(latestVersion);
-    }
+  public abstract <T> TaskChain<T> newChain();
 
-    public abstract <T> TaskChain<T> newChain();
+  public abstract <T> TaskChain<T> newSharedChain(String name);
 
-    public abstract <T> TaskChain<T> newSharedChain(String name);
-
-    public abstract IVaultLoader getVaultLoader();
+  public abstract IVaultLoader getVaultLoader();
 }
--- core/src/main/java/software/bigbade/playervaults/BetterPlayerVaults.java
+++ core/src/main/java/software/bigbade/playervaults/BetterPlayerVaults.java
@@ -3,6 +3,10 @@ package software.bigbade.playervaults;
 import co.aikar.taskchain.BukkitTaskChainFactory;
 import co.aikar.taskchain.TaskChain;
 import co.aikar.taskchain.TaskChainFactory;
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
+import java.util.logging.Level;
 import lombok.Getter;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
@@ -25,126 +29,143 @@ import software.bigbade.playervaults.taskchain.ActionChain;
 import software.bigbade.playervaults.utils.CompressionUtil;
 import software.bigbade.playervaults.utils.FileUtils;
 
-import java.io.File;
-import java.net.URL;
-import java.util.Objects;
-import java.util.logging.Level;
-
 public class BetterPlayerVaults extends PlayerVaults {
-    private static final int MC_VERSION = Integer.parseInt(Bukkit.getVersion().split("\\.")[1]);
+  private static final int MC_VERSION =
+      Integer.parseInt(Bukkit.getVersion().split("\\.")[1]);
 
-    private TaskChainFactory taskChainFactory;
-    private FileConfiguration configuration;
-    @Getter
-    private IVaultLoader vaultLoader;
-    private IVaultManager vaultManager;
+  private TaskChainFactory taskChainFactory;
+  private FileConfiguration configuration;
+  @Getter private IVaultLoader vaultLoader;
+  private IVaultManager vaultManager;
 
-    //Required for MockBukkit
-    public BetterPlayerVaults() {
-        super();
+  // Required for MockBukkit
+  public BetterPlayerVaults() { super(); }
+
+  // Required for MockBukkit
+  protected BetterPlayerVaults(JavaPluginLoader loader,
+                               PluginDescriptionFile description,
+                               File dataFolder, File file) {
+    super(loader, description, dataFolder, file);
+  }
+
+  private static URL getDownload(String name) {
+    if (name.equals("mysql")) {
+      name = "MySQL";
+    } else if (name.equals("mongo")) {
+      name = "Mongo";
     }
+    String link =
+        "https://github.com/BigBadE/BetterPlayerVaults/releases/download/" +
+        getLatestVersion() + "/BetterPlayerVaults-" + name + "-" +
+        getLatestVersion() + ".jar";
+    System.out.println("Link: " + link);
+    return FileUtils.getURL(link);
+  }
 
-    //Required for MockBukkit
-    protected BetterPlayerVaults(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
-        super(loader, description, dataFolder, file);
+  @Override
+  public void onEnable() {
+    saveDefaultConfig();
+
+    setPluginLogger(getLogger());
+
+    configuration = getConfig();
+
+    taskChainFactory = BukkitTaskChainFactory.create(this);
+
+    initVersion();
+
+    new TaskChainFactoryImpl(this);
+
+    new ActionChain()
+        .async(() -> {
+          MessageManager messageManager = new MessageManager();
+
+          messageManager.loadMessages(getDataFolder());
+          messageManager.setMainLanguage(
+              configuration.getString("language", "english"));
+
+          loadVaultLoader();
+        })
+        .execute();
+
+    if (configuration.getBoolean("stats", true) &&
+        !Metrics.class.getPackage().getName().equals("org.bstats.bukkit")) {
+      new MetricsManager(this);
     }
+  }
 
-    private static URL getDownload(String name) {
-        if (name.equals("mysql")) {
-            name = "MySQL";
-        } else if (name.equals("mongo")) {
-            name = "Mongo";
+  @Override
+  public <T> TaskChain<T> newChain() {
+    return taskChainFactory.newChain();
+  }
+
+  @Override
+  public <T> TaskChain<T> newSharedChain(String name) {
+    return taskChainFactory.newSharedChain(name);
+  }
+
+  private void loadVaultLoader() {
+    String loader =
+        Objects
+            .requireNonNull(configuration.getString(
+                "save-type", (MC_VERSION >= 14) ? "persistent" : "flatfile"))
+            .toLowerCase();
+    Objects.requireNonNull(loader);
+    LoaderFactory loaderFactory = new LoaderFactory(this);
+    CompressionUtil.setCompressionLevel(
+        getConfig().getInt("compression-level", 3));
+    if (!loaderFactory.checkName(loader, MC_VERSION)) {
+      getPluginLogger().log(
+          Level.SEVERE,
+          "Invalid saveType set! Options are: flatfile, persistent (1.14+), mysql, mongodb");
+      Bukkit.getPluginManager().disablePlugin(this);
+    } else {
+      if (loader.equals("mysql") || loader.equals("mongo")) {
+        LibraryLoader libraryLoader =
+            new LibraryLoader(getDataFolder().getAbsolutePath(), getConfig());
+        libraryLoader.loadLibrary(loader,
+                                  BetterPlayerVaults.getDownload(loader));
+        ConfigurationSection section =
+            configuration.getConfigurationSection("database");
+        if (section == null) {
+          section = configuration.createSection("database");
         }
-        String link = "https://github.com/BigBadE/BetterPlayerVaults/releases/download/" + getLatestVersion() + "/BetterPlayerVaults-" + name + "-" + getLatestVersion() + ".jar";
-        System.out.println("Link: " + link);
-        return FileUtils.getURL(link);
+        vaultLoader = libraryLoader.getVaultLoader(section);
+        finishLoading();
+      } else {
+        vaultLoader = loaderFactory.getVaultLoader(loader);
+      }
+      finishLoading();
+    }
+  }
+
+  private void finishLoading() {
+    if (vaultLoader == null) {
+      getPluginLogger().log(
+          Level.SEVERE,
+          "Error during initialization: Could not create vault loader.");
+      Bukkit.getPluginManager().disablePlugin(this);
     }
 
-    @Override
-    public void onEnable() {
-        saveDefaultConfig();
+    vaultManager = new VaultManager(vaultLoader);
 
-        setPluginLogger(getLogger());
-
-        configuration = getConfig();
-
-        taskChainFactory = BukkitTaskChainFactory.create(this);
-
-        initVersion();
-
-        new TaskChainFactoryImpl(this);
-
-        new ActionChain().async(() -> {
-            MessageManager messageManager = new MessageManager();
-
-            messageManager.loadMessages(getDataFolder());
-            messageManager.setMainLanguage(configuration.getString("language", "english"));
-
-            loadVaultLoader();
-        }).execute();
-
-        if (configuration.getBoolean("stats", true) && !Metrics.class.getPackage().getName().equals("org.bstats.bukkit")) {
-            new MetricsManager(this);
-        }
+    if (!isEnabled()) {
+      return;
     }
 
-    @Override
-    public <T> TaskChain<T> newChain() {
-        return taskChainFactory.newChain();
+    Bukkit.getPluginManager().registerEvents(
+        new VaultCloseListener(vaultManager), this);
+    Objects.requireNonNull(getCommand("playervault"))
+        .setExecutor(new VaultCommand(vaultManager));
+    Objects.requireNonNull(getCommand("clearvault"))
+        .setExecutor(new ClearCommand(vaultManager));
+  }
+
+  @Override
+  public void onDisable() {
+    if (vaultManager == null) {
+      return;
     }
-
-    @Override
-    public <T> TaskChain<T> newSharedChain(String name) {
-        return taskChainFactory.newSharedChain(name);
-    }
-
-    private void loadVaultLoader() {
-        String loader = Objects.requireNonNull(configuration.getString("save-type", (MC_VERSION >= 14) ? "persistent" : "flatfile")).toLowerCase();
-        Objects.requireNonNull(loader);
-        LoaderFactory loaderFactory = new LoaderFactory(this);
-        CompressionUtil.setCompressionLevel(getConfig().getInt("compression-level", 3));
-        if (!loaderFactory.checkName(loader, MC_VERSION)) {
-            getPluginLogger().log(Level.SEVERE, "Invalid saveType set! Options are: flatfile, persistent (1.14+), mysql, mongodb");
-            Bukkit.getPluginManager().disablePlugin(this);
-        } else {
-            if (loader.equals("mysql") || loader.equals("mongo")) {
-                LibraryLoader libraryLoader = new LibraryLoader(getDataFolder().getAbsolutePath(), getConfig());
-                libraryLoader.loadLibrary(loader, BetterPlayerVaults.getDownload(loader));
-                ConfigurationSection section = configuration.getConfigurationSection("database");
-                if (section == null) {
-                    section = configuration.createSection("database");
-                }
-                vaultLoader = libraryLoader.getVaultLoader(section);
-                finishLoading();
-            } else {
-                vaultLoader = loaderFactory.getVaultLoader(loader);
-            }
-            finishLoading();
-        }
-    }
-
-    private void finishLoading() {
-        if (vaultLoader == null) {
-            getPluginLogger().log(Level.SEVERE, "Error during initialization: Could not create vault loader.");
-            Bukkit.getPluginManager().disablePlugin(this);
-        }
-
-        vaultManager = new VaultManager(vaultLoader);
-
-        if (!isEnabled()) {
-            return;
-        }
-
-        Bukkit.getPluginManager().registerEvents(new VaultCloseListener(vaultManager), this);
-        Objects.requireNonNull(getCommand("playervault")).setExecutor(new VaultCommand(vaultManager));
-        Objects.requireNonNull(getCommand("clearvault")).setExecutor(new ClearCommand(vaultManager));
-    }
-
-    @Override
-    public void onDisable() {
-        if (vaultManager == null) {
-            return;
-        }
-        vaultManager.clearVaults();
-    }
+    vaultManager.clearVaults();
+  }
 }
